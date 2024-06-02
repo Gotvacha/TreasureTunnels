62818e45a1425a07ff231f833d6b4afffd903a12
Gotvacha <vladimir.s.barev.2020@elsys-bg.org>
2024-06-02 14:45:58 +0300

    base structure

--- src/main/java/model/Minor.java
+++ src/main/java/model/Minor.java
@@ -1,4 +1,7 @@
 package model;
 
 public class Minor {
+    public void sayHello() {
+        System.out.println("Hello, World!");
+    }
 }
---
+++ src/main/java/view/Main.java
@@ -0,0 +1,10 @@
+package view;
+
+import model.Minor;
+
+public class Main {
+    public static void main(String[] args) {
+        Minor minor = new Minor();
+        minor.sayHello();
+    }
+}
