ea8a9def72c9ce09df79505845dd34841d8dde5a
Gotvacha <101015518+Gotvacha@users.noreply.github.com>
2024-06-09 12:07:30 +0300

    treasury commit

--- src/main/java/model/Treasury.java
+++ src/main/java/model/Treasury.java
@@ -1,20 +1,28 @@
-package org.example.models;
+package model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class Treasury {
     private static Treasury instance;
     private static int money = 0;
+
+    public Treasury(int money){
+        Treasury.money = money;
+    }
 
     public static Treasury getInstance(){
         if(instance == null){
             instance = new Treasury();
         }
-
         return instance;
     }
 
     public void addMoney(int minerMoney){
         money += minerMoney;
     }
-
 
 }
